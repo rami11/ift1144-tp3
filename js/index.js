c86910903b1aea20bb099345dd24357e53eaf77a
Rami Serapian <rami.serapian@gmail.com>
2019-03-24 11:52:02 -0400

    Use dishImgs object rather than sperate path variables

--- js/index.js
+++ js/index.js
@@ -1,19 +1,21 @@
-const saladImgPath = "img/salade.jpg";
-const spaghettiImgPath = "img/spaghetti.jpg";
-const iceCreamImgPath = "img/cremeglacee.jpg";
+const dishImgs = {
+  saladImgPath: "img/salade.jpg",
+  spaghettiImgPath: "img/spaghetti.jpg",
+  iceCreamImgPath: "img/cremeglacee.jpg"
+};
 
 function onDishRadioClick(dishRadio) {
   switch (dishRadio.id) {
     case "radio-salad":
-      document.getElementById("img-dish").src = saladImgPath;
+      document.getElementById("img-dish").src = dishImgs.saladImgPath;
       break;
     case "radio-spaghetti":
-      document.getElementById("img-dish").src = spaghettiImgPath;
+      document.getElementById("img-dish").src = dishImgs.spaghettiImgPath;
       break;
     case "radio-icecream":
-      document.getElementById("img-dish").src = iceCreamImgPath;
+      document.getElementById("img-dish").src = dishImgs.iceCreamImgPath;
       break;
     default:
-      document.getElementById("img-dish").src = saladImgPath;
+      document.getElementById("img-dish").src = dishImgs.saladImgPath;
   }
 }
