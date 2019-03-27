12949eb14ad5f6e28afc13a9ea28127a63fa360c
Rami Serapian <rami.serapian@gmail.com>
2019-03-27 15:28:55 -0400

    Finish Part 2 (could do some refactoring!)

--- js/index.js
+++ js/index.js
@@ -27,26 +27,48 @@ $(document).ready(function() {
   function onFormSubmit() {
     let nameValue = $("#text-name").val();
     let levelValue = $("#text-level").val();
+
+    $("#result-part2").css({ color: "#0f0" });
     switch (levelValue) {
       case "1":
         $("#result-part2").html(
-          "<span style='font-size: 30px'>" + nameValue + "</span>"
+          "<h1>Bonjour " + nameValue + " niveau=" + levelValue + "</h1>"
         );
         break;
       case "2":
+        $("#result-part2").html(
+          "<h2>" + nameValue + " niveau=" + levelValue + "</h2>"
+        );
         break;
       case "3":
+        $("#result-part2").html(
+          "<h3>" + nameValue + " niveau=" + levelValue + "</h3>"
+        );
         break;
       case "4":
+        $("#result-part2").html(
+          "<h4>" + nameValue + " niveau=" + levelValue + "</h4>"
+        );
         break;
       case "5":
+        $("#result-part2").html(
+          "<h5>" + nameValue + " niveau=" + levelValue + "</h5>"
+        );
         break;
       case "6":
+        $("#result-part2").html(
+          "<h6>" + nameValue + " niveau=" + levelValue + "</h6>"
+        );
         break;
       case "7":
+        $("#result-part2").html(
+          "<h7>" + nameValue + " niveau=" + levelValue + "</h7>"
+        );
         break;
       default:
-        $("#result-part2").html("Invalid Entry!");
+        $("#result-part2")
+          .css({ color: "#f00" })
+          .html("<p>Erreur le niveau doit être 1 et 6</p>");
     }
   }
 
